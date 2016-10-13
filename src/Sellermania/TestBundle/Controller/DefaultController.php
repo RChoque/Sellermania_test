6c3ea8ae36473a2432473ec0d53da02a341d4e03
Rémi <remi.choque@gmail.com>
2016-10-13 22:55:17 +0200

    creation du Bundle

---
+++ src/Sellermania/TestBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Sellermania\TestBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('SellermaniaTestBundle:Default:index.html.twig');
+    }
+}
---
+++ src/Sellermania/TestBundle/SellermaniaTestBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Sellermania\TestBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SellermaniaTestBundle extends Bundle
+{
+}
